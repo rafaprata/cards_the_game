49176641aab2ace9658161f137d11d78abef4b8e
Rafael Prata <rafaelprata@pop-os.localdomain>
2021-03-17 15:18:55 -0300

    melhorando o código das funcoes

--- baralho.py
+++ baralho.py
@@ -2,12 +2,10 @@ import random
 
 base = {'naipe':['PAUS', 'COPAS', 'ESPADA', 'OUROS'], 
     'numero':['A', '2', '3', '4', '5', '6', '7', 'J', 'Q', 'K']} #Colocar o 8, 9, 10 se for usar o baralho inteiro
-baralho = list()
-jogador1 = list()
-computador = list()
-carta = {}
 
-def embaralhar(baral):
+def embaralhar():
+    baral = list()
+    carta = dict()
     n = len(baral)
     while len(baral) <= 39: #Se for usar o baralho inteiro <= 51
         n = len(baral)
@@ -20,21 +18,24 @@ def embaralhar(baral):
                 elif v == carta:
                     baral.pop()
                     break
+    return baral
 
-def distribuir(baralho, jogador):
+def distribuir(baralho):
+    jogador = list()
     for i in range(3):
         n = len(baralho)
         jogador.append(baralho[n-1])
         baralho.pop()
+    return jogador
 
 def coringa(baralho):
     coringa = baralho[len(baralho)-1]
     baralho.pop()
     return coringa
 
-embaralhar(baralho)
-distribuir(baralho, jogador1)
-distribuir(baralho, computador)
+baralho = embaralhar()
+jogador1 = distribuir(baralho)
+computador = distribuir(baralho)
 coringa = coringa(baralho)
 print(baralho)
 print(jogador1)
