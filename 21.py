9234b59d8ba5eefd2acaae278201a4c32c0ff546
Rafael Prata <rafaelprata@pop-os.localdomain>
2021-03-22 13:57:41 -0300

    adicionando a funcao de comprar carta

--- 21.py
+++ 21.py
@@ -1,3 +1,5 @@
+from baralho import embaralhar_21
+
 def distribuir(baralho):
     jogador = list()
     for i in range(3):
@@ -5,3 +7,16 @@ def distribuir(baralho):
         jogador.append(baralho[n-1])
         baralho.pop()
     return jogador
+
+def compar(jogador):
+    jogador.append(baralho_21[0])
+    baralho_21.pop(0) 
+
+
+baralho_21 = embaralhar_21()
+jogador1 = distribuir(baralho_21)
+computador = distribuir(baralho_21)
+
+print(jogador1)
+compar(jogador1)
+print(jogador1)
