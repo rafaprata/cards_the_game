421d656959e10e6e63179eef61ec0f588cdc5e0e
Rafael Prata <rafaelprata@pop-os.localdomain>
2021-03-22 14:11:09 -0300

    funcao para 21

--- baralho.py
+++ baralho.py
@@ -3,7 +3,7 @@ from random import randint
 base = {'naipe':['PAUS', 'COPAS', 'ESPADA', 'OUROS'], 
     'numero':['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']} #Colocar o 8, 9, 10 se for usar o baralho inteiro
 
-def embaralhar():
+def embaralhar_21():
     baral = list()
     carta = dict()
     n = len(baral)
@@ -11,12 +11,12 @@ def embaralhar():
         n = len(baral)
         carta['naipe'] = base['naipe'][randint(0,3)]
         carta['numero'] = base['numero'][randint(0,12)]
-        #if carta['numero'] in 'JQK':
-        #    carta['valor'] = 10
-        #elif carta['numero'] in 'A':
-        #    carta['valor'] = 1
-        #else:
-        #    carta['valor'] = int(carta['numero'])
+        if carta['numero'] in 'JQK':
+            carta['valor'] = 10
+        elif carta['numero'] in 'A':
+            carta['valor'] = 1
+        else:
+            carta['valor'] = int(carta['numero'])
 
         baral.append(carta.copy())
         for i, v in enumerate(baral):
@@ -56,6 +56,3 @@ def embaralhar_truco():
                     baralho.pop()
                     break
     return baralho
-
-baralho = embaralhar_truco()
-print(baralho)
