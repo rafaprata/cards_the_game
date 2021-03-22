1f9f436771fbc2977e5ebafc1a6c632276d7f44c
Rafael Prata <rafaelprata@pop-os.localdomain>
2021-03-22 13:40:50 -0300

    mantendo o baralho completo para jogar

--- baralho.py
+++ baralho.py
@@ -1,16 +1,23 @@
 from random import randint
 
 base = {'naipe':['PAUS', 'COPAS', 'ESPADA', 'OUROS'], 
-    'numero':['A', '2', '3', '4', '5', '6', '7', 'Q', 'J', 'K']} #Colocar o 8, 9, 10 se for usar o baralho inteiro
+    'numero':['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']} #Colocar o 8, 9, 10 se for usar o baralho inteiro
 
 def embaralhar():
     baral = list()
     carta = dict()
     n = len(baral)
-    while len(baral) <= 39: #Se for usar o baralho inteiro <= 51
+    while len(baral) <= 51: #Se for usar o baralho inteiro <= 51
         n = len(baral)
         carta['naipe'] = base['naipe'][randint(0,3)]
-        carta['numero'] = base['numero'][randint(0,9)] #Se for usar o baralho inteiro, até 12
+        carta['numero'] = base['numero'][randint(0,12)] #Se for usar o baralho inteiro, até 12
+        if carta['numero'] in 'JQK':
+            carta['valor'] = 10
+        elif carta['numero'] in 'A':
+            carta['valor'] = 1
+        else:
+            carta['valor'] = int(carta['numero'])
+
         baral.append(carta.copy())
         for i, v in enumerate(baral):
                 if i == len(baral)-1:
@@ -19,13 +26,3 @@ def embaralhar():
                     baral.pop()
                     break
     return baral
-
-baralho = embaralhar()
-jogador1 = distribuir(baralho)
-computador = distribuir(baralho)
-coringa = coringa(baralho)
-jogar(jogador1)
-
-print(f'Jogador 1 - {jogador1}')
-print(f'Computador - {computador}')
-print(coringa)
